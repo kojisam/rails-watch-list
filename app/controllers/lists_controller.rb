7000f0f712a0d6ff055fbcdc505edbff11626cd1
Koji Samuels <kojisamuels@gmail.com>
2022-10-22 17:12:43 +0100

    rails-watch-list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'List Saved'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # def set_lists
+  #   @lists = List.find(params[:id])
+  # end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:create, :index, :show, :new] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
